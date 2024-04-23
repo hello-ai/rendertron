8a8ac2fa5e140b5aa21cef35f88d9f52db906566
Kazato Sugimoto <uiureo@gmail.com>
2024-04-23 15:39:44 +0900

    Tweak max-age

--- src/renderer.ts
+++ src/renderer.ts
@@ -244,7 +244,7 @@ export class Renderer {
       ? new Map(JSON.parse(customHeaders))
       : new Map();
 
-    headers.set('cloudflare-cdn-cache-control', 'max-age=1800');
+    headers.set('cloudflare-cdn-cache-control', 'max-age=600');
 
     return {
       status: statusCode,
